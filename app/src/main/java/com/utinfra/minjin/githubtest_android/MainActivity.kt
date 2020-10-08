d072b217c54fd0c5171bdae4baaf886f55b3d211
UtKim_Min_Jin <gminjin970@gmail.com>
2020-10-08 11:15:16 +0900

    첫번째 커밋

---
+++ app/src/main/java/com/utinfra/minjin/githubtest_android/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.utinfra.minjin.githubtest_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var GitTestNumber = 0
+
+        
+
+
+
+
+
+
+    }
+}
